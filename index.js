506e4e344b18a3d61426f790d65cdabd518e970b
mevinyanuar <mmevinalifviyanuar@gmail.com>
2019-09-04 00:54:01 +0700

    project javascript calculator console

---
+++ index.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const digitA = 27.3
+const digitB = 120.5
+
+const resultAddition = digitA + digitB
+const resultSubstraction = digitA - digitB
+const resultMultiplication = Math.floor(digitA * digitB)
+const resultDivison = Math.round(digitA / digitB)
+
+console.log(digitA)
+console.log(digitB)
+
+console.log(resultAddition)
+console.log(resultSubstraction)
+console.log(resultMultiplication)
+console.log(resultDivison)
